5e874ac060fd5c90b32d869e1e02c9a5bd1dd2a8
Harun-or-rashid-hybri <hrashid@hybri.tech>
2024-11-05 19:31:55 +0600

    them toggle button implementing

---
+++ process_form.php
@@ -0,0 +1,14 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    echo "Hello World";
+    // Retrieve form data
+    // $name = $_POST["name"];
+    // $email = $_POST["email"];
+    // $message = $_POST["message"];
+
+    // // Display the submitted data
+    // echo "Name: " . $name . "<br>";
+    // echo "Email: " . $email . "<br>";
+    // echo "Message: " . $message . "<br>";
+}
+?>
